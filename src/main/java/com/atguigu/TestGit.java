1a63c694fa8a6dac99319304e8104d1e5ab67eaa
YAY <yay@qq.com>
2023-02-02 18:09:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/TestGit.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author ayan
+ * @create 2023-02-02-17:41
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+    }
+}
