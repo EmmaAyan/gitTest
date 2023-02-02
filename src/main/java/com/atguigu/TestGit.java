8ddab033c38629dc6b3f21baf03a4701b95ab8fd
EmmaAyan <124229848+EmmaAyan@users.noreply.github.com>
2023-02-02 19:03:53 +0800

    Update TestGit.java

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -11,5 +11,6 @@ public class TestGit {
         System.out.println("hello git1");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("git repository");
     }
 }
